a0c5a5f2104c8b46a84d0f93ffb78f360b4b2e05
annzheng93 <33013527+annzheng93@users.noreply.github.com>
2017-11-03 15:20:25 -0400

    file upload
    file upload

---
+++ project4 2/Point.cpp
@@ -0,0 +1,35 @@
+/**
+ * Point.cpp
+ *
+ * EECS 183, Fall 2017
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Point.h"
+
+// for the declaration of DIMENSION
+#include "utility.h"
+
+// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins,  Point& pt)
+{
+    pt.read(ins);
+    return ins;
+}
+
+ostream& operator<< (ostream& outs, Point pt)
+{
+    pt.write(outs);
+    return outs;
+}
